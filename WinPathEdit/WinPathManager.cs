43a5db81ecfe9018e78129ee1b245f917b9e9ac2
Ytrog <hisbrucker@gmail.com>
2015-03-07 17:08:35 +0100

    Added WinPathManager.cs

---
+++ WinPathEdit/WinPathManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinPathEdit
+{
+    /// <summary>
+    /// Manages the Windows Path variable
+    /// </summary>
+    class WinPathManager
+    {
+
+        public string PathVar { get; set; }
+
+        public string[] Values { get; set; }
+
+        public WinPathManager()
+        {
+            PathVar = Environment.GetEnvironmentVariable("path", EnvironmentVariableTarget.Process);
+            Values = PathVar.Split(';');
+        }
+
+
+
+    }
+}
